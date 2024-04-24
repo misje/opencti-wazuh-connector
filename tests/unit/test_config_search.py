45ee0ba56e992a8e1bbcecc6552aed9edfa419e8
Andreas Misje <amisje@gmail.com>
2024-04-24 20:29:11 +0200

    Test search option validators

---
+++ tests/unit/test_config_search.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+import os
+import sys
+import pytest
+from pydantic import ValidationError
+
+sys.path.insert(0, os.path.abspath("../../src"))
+from wazuh.search_config import DirSearchOption, FileSearchOption, SearchConfig
+
+
+@pytest.fixture(
+    params=[
+        "case-insensitive",
+    ]
+)
+def filesearch_opts_requiring_regexp(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=[
+        "match-subdirs",
+        "search-filenames",
+        "case-insensitive",
+        "ignore-trailing-slash",
+    ]
+)
+def dirsearch_opts_requiring_regexp(request):
+    return request.param
+
+
+def test_default_nothrow():
+    SearchConfig()
+
+
+def test_file_noregexp_throw(filesearch_opts_requiring_regexp):
+    with pytest.raises(ValidationError):
+        SearchConfig(filesearch_options=filesearch_opts_requiring_regexp)
+
+
+def test_dir_noregexp_throw(dirsearch_opts_requiring_regexp):
+    with pytest.raises(ValidationError):
+        SearchConfig(dirsearch_options=dirsearch_opts_requiring_regexp)
