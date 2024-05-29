dd938db67e96c116db0a9956a5d1637cd9a71a47
Andreas Misje <amisje@gmail.com>
2024-05-29 17:39:11 +0200

    Add a sleep when acting on indicators
    This hard-coded delay is hopefully enough to give OpenCTI enough time to
    created based-on relationships when creating an indicator in the
    platform from an observable. Related to #11.

--- src/wazuh/wazuh.py
+++ src/wazuh/wazuh.py
@@ -2,6 +2,7 @@ import json
 import stix2
 import ipaddress
 import logging
+import time
 from .config import Config
 from .opensearch import OpenSearchClient
 from pycti import (
@@ -221,6 +222,10 @@ class WazuhConnector:
             entity_type = "vulnerability"
         # Support looking up observables based on indicatorss:
         elif data["entity_id"].startswith("indicator--"):
+            log.info(
+                "Waiting 3 seconds for indicator based-on relationships to be ingested before processing"
+            )
+            time.sleep(3)
             ind = self.helper.api.indicator.read(id=data["entity_id"])
             ind_obs = ind["observables"] if ind and "observables" in ind else []
             # TODO: In some distant feature, with a STIX shifter implementation
