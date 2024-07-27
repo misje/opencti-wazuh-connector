1e43bd0e5c038735cee0d9b7893f20836f1fba4f
Andreas Misje <amisje@gmail.com>
2024-07-27 19:50:26 +0200

    Use OpenCTI version 6.2.x

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,13 +57,13 @@ autodoc_pydantic_model_show_json_error_strategy = "coerce"
 
 # -- extlinks options --------------------------------------------------------
 extlinks = {
-    "octiu": ("https://docs.opencti.io/6.1.X/usage/%s", "OpenCTI usage documentation"),
+    "octiu": ("https://docs.opencti.io/6.2.X/usage/%s", "OpenCTI usage documentation"),
     "octia": (
-        "https://docs.opencti.io/6.1.X/administration/%s",
+        "https://docs.opencti.io/6.2.X/administration/%s",
         "OpenCTI administration documentation",
     ),
     "octid": (
-        "https://docs.opencti.io/6.1.X/deployment/%s",
+        "https://docs.opencti.io/6.2.X/deployment/%s",
         "OpenCTI deployment documentation",
     ),
     "octigh": (
